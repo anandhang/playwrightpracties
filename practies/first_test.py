7c9a831e550d49394b1c954cef48dfb90cf8cc6d
anandhang <anantsmail@gmail.com>
2025-12-31 16:25:04 +0530

    leadcode check

--- practies/first_test.py
+++ practies/first_test.py
@@ -1,8 +1,28 @@
 from playwright.sync_api import  sync_playwright
 
+"""
 with sync_playwright() p:
     browser = p.chromium.launch(headless=False)
     page = browser.new_page()
     page.goto("https://example.com")
     print(page.title())
     browser.close()
+"""
+
+def addtwo():
+    nums = [2,7,11,15]
+    target = 9
+    rnums = []
+    for n in nums:
+        i = 0
+        firstItem = n
+        for m in nums:
+            j = 0
+            secItem = m
+            if firstItem + secItem == target and i != j:
+                rnums[0] = firstItem
+                rnums[1] = secItem
+                return rnums
+    
+result = addtwo()
+print(result)
--- practies/test_demo1.py
+++ practies/test_demo1.py
@@ -1,2 +1,79 @@
-def test_example1():
-    assert 1+1 == 4
+def addtwo():
+    nums = [3,2,4]
+    target = 6
+    rnums = []
+    i = 0
+    for n in nums:
+        i = i
+        j = 0
+        for m in nums:
+            j = j
+            firstItem = n
+            secItem = m
+            if firstItem + secItem == target and i != j:
+                rnums.append(i)
+                rnums.append(j)           
+                return rnums
+            j=j+1
+        i=i+1
+    
+#result = addtwo()
+#print(result)
+
+def palindromenumber():
+    x = -121
+    strightnum = str(x)
+    reversenum = str(x)[::-1]
+    if strightnum == reversenum:
+        return True
+    else:
+        return False
+#result1 = palindromenumber()
+#print(result1)
+'''
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        strnum = str(s)
+        count = 0
+        resdic = {"M" : 1000, "D" : 500, "C" : 100, "L" : 50, "X" : 10, "V" : 5, "I" : 1}
+        a = 0
+        for n in strnum[::-1]:
+            b = resdic.get(n) 
+            a = int(a) + int(b)
+        return a
+'''
+def roman(s):
+    resdic = {"M" : 1000, "D" : 500, "C" : 100, "L" : 50, "X" : 10, "V" : 5, "I" : 1}
+    for i in range(len(s)):
+        
+
+        n = ""  
+        i = i + r      
+        if i >= (len(s)):
+            break
+        if s[i] == "C" and i + 1 < (len(s)):
+            if s[i+1] == "M":
+                n = "CM"
+                r = r + 1
+        elif s[i] == "X" and i + 1 < (len(s)):
+            if s[i+1] == "C":
+                n = "XC"
+                r = r + 1  
+        elif s[i] == "I" and i + 1 < (len(s)):
+            if s[i+1] == "V":
+                n = "IV"
+                r = r + 1  
+            elif s[i+1] == "X":
+                    n = "IX"
+                    r = r + 1
+        if n == "":
+            n = s[i]
+        b = resdic.get(n) 
+        a = int(a) + int(b) 
+    return a
+result = roman("IX")#MCMXCIV#LVIII
+print(result)
